4c532f1c372865f9e6b35ec6afb63ec7f5361d27
Gabriel <102483480+cdmlonely802@users.noreply.github.com>
2022-05-29 19:52:34 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let display = document.querySelector('#display')
+
+let minutos = document.querySelector('#minutos')
+let segundos = document.querySelector('#segundos')
+let comecar = document.querySelector('#comecar')
+
+
+let cronometroSeg;
+
+let minutoAtual;
+
+let segundoAtual;
+
+let interval;
+
+for(let i = 0; i <= 60; i++){
+    minutos.innerHTML+='<option value="'+i+'">'+i+'</option>';
+}
+
+for(let i = 1; i <= 60; i++){
+    segundos.innerHTML+='<option value="'+i+'">'+i+'</option>';
+}
+
+comecar.addEventListener('click', function(){
+    minutoAtual = minutos.value;
+    segundoAtual = segundos.value;
+    
+    display.childNodes[1].innerHTML = minutoAtual + ":"+segundoAtual;
+
+    interval = setInterval(function(){
+
+        segundoAtual--;
+        if (segundoAtual <= 0){
+
+            if(minutoAtual > 0){
+
+                minutoAtual--;
+                segundoAtual = 59;
+            }else{
+
+                alert("Acabou!");
+                document.querySelector('#sound').play();
+                clearInterval(interval); 
+
+        }
+
+        }
+        display.childNodes[1].innerHTML = minutoAtual + ":"+segundoAtual;
+
+
+    },1000);
+})
